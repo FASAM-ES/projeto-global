488ee24d8d2a7a044b59852617f695a701d4698a
fredgyn <frederikooliviera@gmail.com>
2014-12-06 15:57:14 -0200

    Documentação parcial da classe comentario

--- ProjetoIntegracaoGlobal/src/main/java/br/com/fasam/projetointegracaoglobal/entidades/Comentario.java
+++ ProjetoIntegracaoGlobal/src/main/java/br/com/fasam/projetointegracaoglobal/entidades/Comentario.java
@@ -9,33 +9,53 @@ package br.com.fasam.projetointegracaoglobal.entidades;
  * @author Aluno
  */
 public class Comentario {
- 
+
     String titulo;
     String descricao;
     String usuario;
     String artigo;
-
+    
+    /*
+     * Construtor da classe.
+     */
     public Comentario() {
         titulo = "Primeiro";
-        
+
     }
     
+    /*
+     * Usuário que registrou o comentário
+     * @return usuario
+     */
     public String getUsuario() {
         return usuario;
     }
 
+    /*
+     * Setar um usuário do comentário
+     * @param usuario que registrou o comentário
+     */
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
 
+    /*
+     * Artigo onde foi feito o comentário
+     * @return artigo 
+     */    
     public String getArtigo() {
         return artigo;
     }
 
+    /*
+     * Inseri o artigo do comentário
+     * @param artigo
+     */
     public void setArtigo(String artigo) {
         this.artigo = artigo;
     }
-    
+
+   
     public String getTitulo() {
         return titulo;
     }
